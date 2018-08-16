b60347e428ae9cc67b66689744539ae6626b71ad
Megan Hainline <meg.hainline@gmail.com>
2018-08-15 21:47:13 -0500

    change

--- II CG 4-3/Program.cs
+++ II CG 4-3/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace II_CG_4_3
+namespace CG_4_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter up to 30 names");
+            var names = new string[30];
+            var numberNamesEntered = 0;
+
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("Please enter name #{0}: ", i + 1);
+                var userInput = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(userInput)) break;
+                names[i] = userInput;
+                numberNamesEntered = i;
+            }
+            var rnd = new Random();
+            var winner = rnd.Next(numberNamesEntered);
+
+            Console.WriteLine("{0}Congratulations to {1}!! They won!", Environment.NewLine, names[winner]);
+            Console.WriteLine("{0}The \"Better Luck Next Time\" award goes to: ", Environment.NewLine);
+
+            for (int x = 0; x <= numberNamesEntered; x++)
+            {
+                if (x == winner) continue;
+                Console.WriteLine(names[x]);
+            }
+
+            Console.ReadLine();
         }
     }
+
 }
